2a7aee61bdd90cd116f9c1ead46bf159f13396b0
Nicholai Gallegos <gallegon@oregonstate.edu>
2022-03-15 23:01:56 -0700

    cleanup code

--- threaded_server.py
+++ threaded_server.py
@@ -4,9 +4,9 @@ import socketserver
 import sys
 import json
 import cryptography
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
+import base64
 
+from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
 mutex = threading.Lock()
@@ -17,29 +17,47 @@ CIPERTEXT_OUTPUT_FILE = "log.txt"
 open_file = open("log.txt", 'w')
 open_file.close()
 
-config_file = sys.argv[1]
-with open(config_file, "r") as read_file:
-    data = json.load(read_file)
-    read_file.close()
+# config_file = sys.argv[1]
+# with open(config_file, "r") as read_file:
+#     data = json.load(read_file)
+#     read_file.close()
 
-server_ip = data['server_ip']
-server_port = data['server_port']
-private_key_path = data['private_key_path']
+# server_ip = data['server_ip']
+# server_port = data['server_port']
+# private_key_path = data['private_key_path']
 
-with open(private_key_path, "rb") as key_file:
-    private_key = serialization.load_pem_private_key(
-        key_file.read(),
-        password=None,
+# with open(private_key_path, "rb") as key_file:
+#     private_key = serialization.load_pem_private_key(
+#         key_file.read(),
+#         password=None
+#     )
 
-    )
+def get_config_info(HOST, PORT, private_key):
+    config_file = sys.argv[1]
+    with open(config_file, "r") as read_file:
+        data = json.load(read_file)
+        read_file.close()
+
+    server_ip = data['server_ip']
+    server_port = data['server_port']
+    private_key_path = data['private_key_path']
+
+    with open(private_key_path, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None
+        )
+
 
 def decrypt_message(ciphertext, private_key):
+    #ciphertext = bytes(ciphertext)
     plaintext = private_key.decrypt(
     ciphertext,
+    #padding.PKCS1v15()
     padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA1()),
-        algorithm=hashes.SHA1(),
-        label=None)
+         mgf=padding.MGF1(algorithm=hashes.SHA1()),
+         algorithm=hashes.SHA1(),
+         label=None)
     )
     return plaintext
 
@@ -48,23 +66,17 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         while(1):
             #data = str(self.request.recv(1024), 'ascii')
             data = self.request.recv(1024)
-            #if data == "exit()":
-            #    print("leaving server")
-            #    break
-            # message = data.decode()
-            # cur_thread = threading.current_thread()
-            # Acquire the lock before output file opening file for writing
+            
             mutex.acquire()
             try:
                 ptext = decrypt_message(data, private_key)
                 ctext_output = open("log.txt", 'a')
                 ctext_output.write(f"{data}\n")
                 ctext_output.close()
-                #print(f"{data}")
+                print(str(ptext, 'ascii').strip())
             finally:
                 response = bytes("recieved", 'ascii')
                 self.request.sendall(response)
-                #print(f"{data}\n")
                 mutex.release()
 
         print("Server thread loop exited")
@@ -83,20 +95,6 @@ def client(ip, port, message):
         print("recieved: ()".format(response))
 
 if __name__=="__main__":
-    #HOST, PORT = "127.0.0.1", 51000
-    
-    # config_file = sys.argv[1]
-
-    # with open(config_file, "r") as read_file:
-    #     data = json.load(read_file)
-    #     read_file.close()
-
-    # server_ip = data['server_ip']
-    # server_port = data['server_port']
-
-    #print(f"python3 threaded_server.py {server_ip} {server_port}")
-
-
     HOST, PORT = server_ip, server_port
 
     # Start the server
