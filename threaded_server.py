fbaa8baeea6c540296d5c56461b59300f006691d
Nicholai Gallegos <gallegon@oregonstate.edu>
2022-03-16 13:49:21 -0700

    modifications to threaded server, now read and authenticate associated data

--- threaded_server.py
+++ threaded_server.py
@@ -9,12 +9,17 @@ import base64
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+# Check if ciphertexts are sent in order, for AEAD.  Global variable
+CHECK_TIMESTAMP = 0
+
 mutex = threading.Lock()
 
 MESSAGE_QUEUE = []
 CIPERTEXT_OUTPUT_FILE = "log.txt"
 
-open_file = open("log.txt", 'w')
+decrypted_ctxts = []
+
+open_file = open("log.txt", 'wb')
 open_file.close()
 
 config_file = sys.argv[1]
@@ -32,12 +37,10 @@ with open(private_key_path, "rb") as key_file:
         password=None
     )
 
-
+# Wrapper function for decrypt
 def decrypt_message(ciphertext, private_key):
-    #ciphertext = bytes(ciphertext)
     plaintext = private_key.decrypt(
     ciphertext,
-    #padding.PKCS1v15()
     padding.OAEP(
          mgf=padding.MGF1(algorithm=hashes.SHA1()),
          algorithm=hashes.SHA1(),
@@ -48,16 +51,40 @@ def decrypt_message(ciphertext, private_key):
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         while(1):
-            #data = str(self.request.recv(1024), 'ascii')
-            data = self.request.recv(1024)
-            #print(repr(data))
+            # Get data from the socket
+            self.data = self.request.recv(1024).strip()
+            if self.data == b'':
+                continue
+            #print(repr(self.data))
+
+            #Check if the data has been decrypted.
+            if self.data in decrypted_ctxts:
+                print("Replayed ciphertext: ", end='')
+            else:
+                decrypted_ctxts.append(self.data)
+
             mutex.acquire()
             try:
-                ptext = decrypt_message(data, private_key)
-                ctext_output = open("log.txt", 'a')
-                ctext_output.write(f"{data}\n")
+                ptext = decrypt_message(self.data, private_key)
+                ctext_output = open("log.txt", 'ab')
+                ctext_output.write(self.data)
                 ctext_output.close()
-                print(str(ptext, 'ascii').strip())
+                plaintext_string = str(ptext, encoding='ascii')
+                
+                strings = plaintext_string.split('\n')
+                
+                plaintext_string = strings[0]
+                timestamp = int(strings[1].rstrip('\x00'), 10)
+                
+                global CHECK_TIMESTAMP
+                if timestamp > CHECK_TIMESTAMP:
+                    CHECK_TIMESTAMP = timestamp
+                else:
+                    print("Timestamps not equal, replay likely")
+
+                
+                print(plaintext_string)
+                print(f"Timestamp: {timestamp}")
             finally:
                 response = bytes("recieved", 'ascii')
                 self.request.sendall(response)
@@ -86,9 +113,6 @@ if __name__=="__main__":
 
         server_thread.start()
         print("Server loop running in thread:", server_thread.name)
-        #client(ip, port, "HIIII")
-        #while 1:
-        #    2*2
         server_thread.join()
 
         server.shutdown()
